fd1e040c1c584a5423e826f71b6ce192812b3915
Alejandro Palacios <apalac99@gmail.com>
2024-08-29 10:32:28 -0400

    Update 1

---
+++ script.js
@@ -0,0 +1,106 @@
+
+
+class Producto {
+  constructor(nombre, referencia, precio, imagen) {
+      this.nombre = nombre;
+      this.referencia = referencia;
+      this.precio = precio;
+      this.imagen = imagen;
+  }
+}
+
+const productos = [];
+
+// Cargar productos desde el archivo JSON
+fetch('productos.json')
+  .then(response => {
+      if (!response.ok) {
+          throw new Error('Error al cargar el archivo JSON');
+      }
+      return response.json();
+  })
+  .then(data => {
+      data.forEach(item => {
+          const producto = new Producto(item.nombre, item.referencia, item.precio, item.imagen);
+          productos.push(producto);
+      });
+      mostrarProductos();
+  })
+  .catch(error => console.error('Error:', error));
+
+// Mostrar productos en el HTML
+function mostrarProductos() {
+  const contenedorProductos = document.getElementById('productos');
+
+  for (let i = 0; i < productos.length; i++) {
+      const producto = productos[i];
+      const productElement = document.createElement('div');
+      productElement.classList.add('producto-columna');
+      productElement.innerHTML = `
+          <h2>${producto.nombre}</h2>
+          <img src="images/${producto.imagen}" width="100" height="100">
+          <br> ${producto.referencia} <br>
+          <strong>$${producto.precio}</strong> <br><br>
+          <button class="button button2" onclick="agregarAlCarrito(${producto.precio}, productos[${i}])">Agregar al carrito</button>
+          <div id="total${producto.referencia}" class="totalUnidades">0</div>
+      `;
+      contenedorProductos.appendChild(productElement);
+  }
+}
+
+const botonVaciar = document.getElementById("vaciarCarritoBtn");
+botonVaciar.addEventListener("click", vaciarCarrito);
+
+// Función para agregar al carrito
+function agregarAlCarrito(precio, producto) {
+  let carrito = localStorage.getItem('carrito') ? parseInt(localStorage.getItem('carrito')) : 0;
+  carrito += precio;
+  localStorage.setItem('carrito', carrito);
+  document.getElementById('totalCarrito').innerHTML = `Total: $${carrito}`;
+
+  // Realizar seguimiento del número de veces que se agrega el producto
+  let cantidadProducto = localStorage.getItem(`${producto.referencia}`) ? parseInt(localStorage.getItem(`${producto.referencia}`)) : 0;
+  cantidadProducto++;
+  localStorage.setItem(`${producto.referencia}`, cantidadProducto);
+  document.getElementById(`total${producto.referencia}`).innerHTML = `${cantidadProducto}`;    // cuantas se han elegido en el cuadro de producto
+}
+
+function actualizarTotalCarrito() {
+  let carrito = localStorage.getItem('carrito') ? parseInt(localStorage.getItem('carrito')) : 0;
+  document.getElementById('totalCarrito').innerHTML = `Total: $${carrito}`;
+  document.getElementById(`pagoCompletado`).innerHTML = ``;
+
+  // Actualizar las unidades de cada producto
+  for (let i = 0; i < productos.length; i++) {
+      let referencia = productos[i].referencia;
+      let cantidadProducto = localStorage.getItem(referencia) ? parseInt(localStorage.getItem(referencia)) : 0;
+      document.getElementById(`total${referencia}`).innerHTML = `0`;
+  }
+}
+
+function vaciarCarrito() {
+  localStorage.setItem('carrito', '0');
+  for (let i = 0; i < productos.length; i++) {
+      let referencia = productos[i].referencia;
+      localStorage.setItem(referencia, '0'); // Reiniciar contador de cada producto 
+  }
+  actualizarTotalCarrito(); // Actualizar el total del carrito y las unidades de cada producto
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+let numeroConfirmacion = getRandomInt(99999999999);
+
+const compraCompleta = document.getElementById("pagar");
+compraCompleta.addEventListener("click", confirmarCompra);
+
+function confirmarCompra() {
+  document.getElementById(`pagoCompletado`).innerHTML = `<br><br><img src="images/pay.png" width="50px" class="imagen"><h3>Pago Confirmado</h3><h4>Número de confirmación<br>${numeroConfirmacion}</h4><br><img src="images/gracias.png" width="50px">`;
+}
+
+
+
+window.onload = actualizarTotalCarrito();
+
